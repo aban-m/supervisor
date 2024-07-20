4909d3e3e26c09932aefab1bd426188862fa2355
aban-m <aban.salah.mahmoud@gmail.com>
2024-07-20 23:11:08 +0300

    Changed daemon module structure

--- server/daemon/__init__.py
+++ server/daemon/__init__.py
@@ -1,14 +1,1 @@
-# read configuration file
-from configparser import ConfigParser
-from os.path import exists
 
-CONFIG_PATH = 'config.ini'
-
-# if file does not exist, make it
-if not exists(CONFIG_PATH):
-    open(CONFIG_PATH, 'w').close() # touch it 
-
-config = ConfigParser()
-config.read(CONFIG_PATH)
-
-MAINTAIN_PERIOD = int(config['DEFAULT'].get('MAINTAIN_PERIOD', 60))
